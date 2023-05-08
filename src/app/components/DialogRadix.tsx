edf32f3fae0aae76d8e63f7399f32ff86c0e921b
gioiamicaela <gioiamicaela@gmail.com>
2023-05-08 14:51:16 +0200

    dialog drag

--- src/app/components/DialogRadix.tsx
+++ src/app/components/DialogRadix.tsx
@@ -1,25 +1,37 @@
 import { Dialog } from '@radix-ui/react-dialog';
-import React, { useState } from 'react';
+import React, { MouseEventHandler, useState } from 'react';
 
 interface Props {
   show: boolean;
   onOpenChange: () => void;
   text: string;
+  onMouseDown: React.MouseEventHandler<HTMLDivElement>;
+  position: {
+    x: string;
+    y: string;
+  };
 }
 
-export default function DialogRadix({ show, onOpenChange, text }: Props) {
+export default function DialogRadix({
+  show,
+  onOpenChange,
+  text,
+  onMouseDown,
+  position,
+}: Props) {
   return (
     <>
       <div
         style={{
           position: 'absolute',
-          top: '25%',
-          left: '50%',
+          top: position.y,
+          left: position.x,
           height: '500px',
           width: '1000px',
           zIndex: 9999,
           backgroundColor: 'red',
         }}
+        onMouseDown={onMouseDown}
       >
         <Dialog open={show} onOpenChange={onOpenChange}>
           <h2>Text</h2>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,14 +9,33 @@ const inter = Inter({ subsets: ['latin'] });
 export default function Home() {
   const [show, setShow] = useState(false);
   const [text, setText] = useState('');
+  const [position, setPosition] = useState({ x: '50%', y: '25%' });
 
   const handleOpen = (text: string) => {
     setShow(true);
     setText(text);
+    setPosition({ x: '50%', y: '25%' });
   };
 
   const handleClose = () => {
     setShow(false);
+  };
+
+  const handleDrag = (e: MouseEvent) => {
+    setPosition((prevPosition) => ({
+      x: `calc(${prevPosition.x} + ${e.movementX}px)`,
+      y: `calc(${prevPosition.y} + ${e.movementY}px)`,
+    }));
+  };
+
+  const handleDragStart: React.MouseEventHandler = (e) => {
+    document.addEventListener('mousemove', handleDrag);
+    document.addEventListener('mouseup', handleDragEnd);
+  };
+
+  const handleDragEnd = () => {
+    document.removeEventListener('mousemove', handleDrag);
+    document.removeEventListener('mouseup', handleDragEnd);
   };
 
   return (
@@ -26,7 +45,13 @@ export default function Home() {
     >
       <Rectangles onClick={(text) => handleOpen(text)} />
       {show && (
-        <DialogRadix show={show} onOpenChange={handleClose} text={text} />
+        <DialogRadix
+          show={show}
+          onOpenChange={handleClose}
+          text={text}
+          onMouseDown={handleDragStart}
+          position={position}
+        />
       )}
     </main>
   );
